e867d5f8305e4a208ee1bc98c3ccc50fb908935e
brandonmcastillo <brandonmaerina@gmail.com>
2019-05-15 00:54:16 -0700

    Logo Component Made

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css';
+import picture from './Logo.png';
+
+const Logo = () => {
+    return (
+        <div className="ma4 mt0">
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 100 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-Inner">
+                    <img style={{ paddingTop: '5px' }} alt="float" src={picture} />
+                </div>
+            </Tilt>
+        </div>
+    )
+};
+
+export default Logo;
