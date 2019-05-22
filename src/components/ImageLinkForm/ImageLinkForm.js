278e3bb124531c0058b3d03f9d8c426441fafce9
brandonmcastillo <brandonmaerina@gmail.com>
2019-05-21 19:04:24 -0700

    Fix image link form

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = ({ onInputChange, onSubmit }) => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
             <p className="f3">
@@ -10,7 +10,7 @@ const ImageLinkForm = ({ onInputChange, onSubmit }) => {
             <div className="center">
                 <div className="form center pa3 br3 shadow-5">
                     <input onChange={onInputChange} className="f4 pa2 w-70 center" type="text" />
-                    <button onClick={onSubmit} className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>                   
+                    <button onClick={onButtonSubmit} className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>                   
                 </div>
             </div>
         </div>
